3b71290a299cee6df9f187bc7eabb92ba190bc27
Manuel <mrctinio@gmail.com>
2021-04-13 21:30:58 +0800

    [WIP] Created profile view for buyer, Buyer_services controller, changed columns for buyer_service model

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,9 +2,17 @@ class BuyersController < ApplicationController
   def show
     @buyer = Buyer.find(params[:id])
     @categories = Service.distinct(:category).pluck(:category)
+    @categories_count = []
+    @categories.each do |category|
+      count = Service.where(category: category).count
+      @categories_count << { category: category, count: count }
+    end
   end
 
   def filter
     @services = Service.where('lower(category) = ?', params[:category].downcase)
+  end
+
+  def profile
   end
 end
