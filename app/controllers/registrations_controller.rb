c3ab6bbae20b45f620d48c697f82400045e517b2
Rio <rjmssevilla@gmail.com>
2021-04-19 16:53:04 +0400

    fixed heroku deployment

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,20 @@ class RegistrationsController < Devise::RegistrationsController
     when Buyer then buyer_path(@buyer)
     end
   end
+
+  def after_inactive_sign_up_path_for(resource)
+    case resource
+      when Admin then admins_path
+      when Supplier then root_path
+      when Buyer then root_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    case resource
+      when Admin then admins_path
+      when Supplier then root_path
+      when Buyer then root_path
+    end
+  end
 end
