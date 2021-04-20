9cfadf77bf7f4e7a3356d1902843fbd37719d83b
Rio <rjmssevilla@gmail.com>
2021-04-20 12:17:52 +0400

    fixed buyer.rb

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,7 +2,7 @@ class Buyer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
